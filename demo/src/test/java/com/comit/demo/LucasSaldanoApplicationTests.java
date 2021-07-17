e325460315701191568ca8f67080fd57ac4aece4
lucas <lucas@lucas-Lenovo-ideapad-320-15IKB>
2021-07-17 16:42:43 -0300

    Mi primer commit trrabajo final

---
+++ demo/src/test/java/com/comit/demo/LucasSaldanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasSaldanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
