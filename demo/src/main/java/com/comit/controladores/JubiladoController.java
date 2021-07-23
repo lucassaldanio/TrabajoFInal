81cbc02afbfb61fde394b4ea061f3ac793c80dce
lucas <lucas@lucas-Lenovo-ideapad-320-15IKB>
2021-07-23 17:21:52 -0300

    Servicios

--- demo/src/main/java/com/comit/controladores/JubiladoController.java
+++ demo/src/main/java/com/comit/controladores/JubiladoController.java
@@ -1,15 +1,29 @@
 package com.comit.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.comit.demo.modelo.Jubilado;
+import com.comit.servicios.JubiladoService;
 
 @Controller
 @RequestMapping("/jubilado")
 public class JubiladoController { 
+	
+	private final JubiladoService jubiladoService;
+	
+	@Autowired
+	public JubiladoController (JubiladoService jubiladoService) {
+		this.jubiladoService = jubiladoService;
+	}
+	
+	
+	@GetMapping("/crearJubilado")
 	public String crear (@ModelAttribute(name = "jubilado")Jubilado jubilado) {
+		jubiladoService.guardar(jubilado);
 		return "redirect:/";
 	}
 
---
+++ demo/src/main/java/com/comit/servicios/JubiladoService.java
@@ -0,0 +1,13 @@
+package com.comit.servicios;
+
+import org.springframework.stereotype.Service;
+
+import com.comit.demo.modelo.Jubilado;
+
+@Service
+public class JubiladoService {
+
+	public void guardar(Jubilado jubilado) {
+		
+	}
+}
